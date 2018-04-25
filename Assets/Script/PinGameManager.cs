e2ae1b0f487024ff51917907be304cc8c33455f0
Sniper0000 <zhimianwuchina@gmail.com>
2018-04-25 01:28:52 +0100

    something

---
+++ Assets/Script/PinGameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PinGameManager : MonoBehaviour {
+
+    public static int score = 0;
+    public Text scoreText;
+
+    public static int goal = 6;
+
+	// Use this for initialization
+	void Start () {
+        score = goal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = score + "";
+
+        if(score == 0)
+        {
+            if (goal < 14)
+            {
+                StartCoroutine(RestartLv());
+            }
+            else
+            {
+                print("You Win.");
+            }
+        }
+	}
+
+    IEnumerator RestartLv()
+    {
+        yield return new WaitForSeconds(1f);
+        goal += 2;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Script/pin.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pin : MonoBehaviour {
+
+    Rigidbody2D rg;
+    public float speed = 20f;
+    bool isPined = false;
+    bool isGameOver = false;
+
+    spin sp;
+
+	// Use this for initialization
+	void Start () {
+        rg = GetComponent<Rigidbody2D>();
+        sp = GameObject.FindGameObjectWithTag("spin").GetComponent<spin>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!isPined)
+        {
+            rg.MovePosition(rg.position + Vector2.up * speed * Time.deltaTime);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "spin")
+        {
+            isPined = true;
+            this.transform.SetParent(collision.gameObject.transform);
+            if (!isGameOver)
+            {
+                PinGameManager.score -= 1;
+            }
+        }
+
+        if(collision.gameObject.tag == "pin")
+        {
+            Camera.main.backgroundColor = Color.red;
+            StartCoroutine(RestartLv());
+            sp.speed = 50f;
+            isGameOver = true;
+        }
+    }
+
+    IEnumerator RestartLv()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
