34e63bce7653ddf1e7e4c4de657b82425e19ae90
Sniper0000 <zhimianwuchina@gmail.com>
2018-04-25 02:04:18 +0100

    I have gave up a 3d Unity project and tried to work on a 2d game and update the project today

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+    public Text levelText;
+
+    public static int level = 1;
+
+    PinGameManager PGM;
+
+    // Use this for initialization
+    void Start () {
+
+        PGM = GameObject.FindGameObjectWithTag("PinGameManager").GetComponent<PinGameManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        levelText.text = level + "";
+
+        if (PinGameManager.score == 0)
+        {
+            StartCoroutine(RestartLv());
+        }
+
+    }
+
+    IEnumerator RestartLv()
+    {
+        yield return new WaitForSeconds(1f);
+        level += 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
